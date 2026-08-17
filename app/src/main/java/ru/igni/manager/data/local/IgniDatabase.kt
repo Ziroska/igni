c4154906a121a229f61ceb9da58bded733b8cc92
Ziro <109761642+Ziroska@users.noreply.github.com>
2026-08-17 16:47:31 +0300

    fix: prevent destructive database recreation

--- app/src/main/java/ru/igni/manager/data/local/IgniDatabase.kt
+++ app/src/main/java/ru/igni/manager/data/local/IgniDatabase.kt
@@ -44,7 +44,6 @@ abstract class IgniDatabase : RoomDatabase() {
                 "igni_manager.db"
             )
                 .addMigrations(MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5, MIGRATION_5_6, MIGRATION_6_7)
-                .fallbackToDestructiveMigration()
                 .build()
 
         private val MIGRATION_2_3 = object : Migration(2, 3) {
